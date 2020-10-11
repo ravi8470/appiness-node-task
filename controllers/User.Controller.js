ae0e968c47806d18fe09084a74af12db2a5ae469
Ravi Kumar <rvkmr0851@gmail.com>
2020-10-12 01:56:28 +0530

    added check for existing roles and duplicate entries in users collection

--- controllers/User.Controller.js
+++ controllers/User.Controller.js
@@ -1,48 +1,67 @@
+import { create } from 'core-js/fn/object';
 import validator from 'validator';
-import UserModel from '../models/User.model';
+import { RoleModel } from '../models/Roles.model';
+import { UserModel } from '../models/User.model';
 
 
-export const registerUser = async (req, res, next) => {
+export const registerUser = async (req, res) => {
 
-  const data = Object.assign({}, req.body) || {};
+  try {
+    const data = Object.assign({}, req.body) || {};
 
-  if (!data.email || !data.username || !data.password) {
-    return res.status(400).send('Email, password and Username are required fields');
-  }
-
-  if (data.email && !validator.isEmail(data.email)) {
-    return res.status(400).send('Invalid email address.');
-  }
-
-  if (data.username && !validator.isAlphanumeric(data.username)
-    && !validator.isLength(data.username, { min: 8, max: 24 })) {
-    return res.status(400).send('Usernames must be alphanumeric and 8 to 24 chars');
-  }
-
-  if (data.password && !validator.isLength(data.password, { min: 8, max: 24 })) {
-    return res.status(400).send('Password must be  8 - 24 chars');
-  }
-  
-  let totalUserCount = await UserModel.find().count();
-  
-  if(totalUserCount === 0) {
-    data.role = 1; // set the user as admin
-  } else {
-    // check if a role id was provided and it was not equal to admin role id, i.e., 1
-    if(!(typeof data.role === Number) || data.role === 1){
-      return res.status(400).send('Invalid value provided for role');
+    if (!data.email || !data.username || !data.password) {
+      return res.status(400).send('Email, password and Username are required fields');
     }
+
+    if (data.email && !validator.isEmail(data.email)) {
+      return res.status(400).send('Invalid email address.');
+    }
+
+    if (data.username && (
+      !validator.isAlphanumeric(data.username)
+      ||
+      !validator.isLength(data.username, { min: 8, max: 24 })
+    )) {
+      return res.status(400).send('Usernames must be alphanumeric and 8 to 24 chars');
+    }
+
+    if (data.password && !validator.isLength(data.password, { min: 8, max: 24 })) {
+      return res.status(400).send('Password must be  8 - 24 chars');
+    }
+
+    let totalUserCount = await UserModel.estimatedDocumentCount();
+
+    if (totalUserCount === 0) {
+      data.role = 1; // set the user as admin
+    } else {
+      // check if a role id was provided and it was not equal to admin role id, i.e., 1
+      if (!(typeof data.role === "number") || data.role === 1) {
+        return res.status(400).send('Invalid value provided for role');
+      }
+      // check if the provided role id exists in role collection
+      let roleCount = await RoleModel.countDocuments({ id: data.role });
+      if (roleCount !== 1) {
+        return res.status(400).send('No roles matching the given value found');
+      }
+    }
+    // check if username/email already exists
+    let duplicateUserCount = await UserModel.countDocuments({
+      $or: [
+        { username: data.username },
+        { email: data.email }
+      ]
+    });
+    if(duplicateUserCount > 0){
+      return res.status(400).send('User already exists with given parameters');
+    }
+
+    let createdUser = await UserModel.create(data);
+    createdUser = createdUser.toObject();
+    delete createdUser.password;
+    res.status(201).json(createdUser);
+
+  } catch (err) {
+    console.log('Error: ', err);
+    res.status(500).json('Internal Server Error');
   }
-
-  let createdUser = await UserModel.create(data);
-  res.json(createdUser);
-
-  // UserModel.create(data)
-  //   .then(user => {
-  //     res.json(user);
-  //   })
-  //   .catch(err => {
-  //     console.log('Error:', err);
-  //     res.status(500).send('Internal Server Error');
-  //   });
 };
